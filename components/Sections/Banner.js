7ac818a008aa2753aab7c35af23f5a199b913f6e
Gaick Bany <banygaick@gmail.com>
2022-04-09 10:58:29 +0200

    second commit

--- components/Sections/Banner.js
+++ components/Sections/Banner.js
@@ -7,7 +7,7 @@ const Banner = () => {
       <Navbar/>
       <div className="hero__banner container grid">
             <div className="hero__banner__container">
-                <h2 className="hero__banner__title">Need to Service or <br/> repair your car?</h2>
+                <h1 className="hero__banner__title">Need to Service or <br/> repair your car?</h1>
                 <p className="hero__banner__description">Car servicing rationally encounter consequences that extremely painful. Nor again is there anyone who loves or pursues</p>
                 <a href="#" className="hero__banner__button button">Contactez Nous</a>
             </div>
